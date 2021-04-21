3cd5f74b79d944179d3d5dab3a9d594b53edac15
Noel Lang <noel@lang-technology.de>
2021-04-21 23:21:58 +0200

    Add customer creation test

--- src/test/java/de/noellang/customerapi/CustomerControllerTest.java
+++ src/test/java/de/noellang/customerapi/CustomerControllerTest.java
@@ -1,6 +1,9 @@
 package de.noellang.customerapi;
 
+import de.noellang.customerapi.model.Customer;
+import de.noellang.customerapi.payload.CreateCustomerRequest;
 import de.noellang.customerapi.security.JwtTokenProvider;
+import de.noellang.customerapi.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,15 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.text.MessageFormat;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-test.properties")
-public class CustomerControllerTest {
+class CustomerControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -24,19 +28,40 @@ public class CustomerControllerTest {
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
 
+	@Autowired
+	private CustomerService customerService;
+
 	@Test
-	public void shouldReturn403Forbidden() throws Exception {
+	void cantRetrieveCustomersWhenUnauthenticated() throws Exception {
 		mockMvc.perform(get("/v1/customer"))
 				.andDo(print())
 				.andExpect(status().isForbidden());
 	}
 
 	@Test
-	public void shouldReturn200Ok() throws Exception {
+	void canRetrieveCustomersWhenAuthenticated() throws Exception {
 		mockMvc.perform(
 				get("/v1/customer")
 						.header("Authorization", "Bearer " + jwtTokenProvider.generateToken()
 					)).andExpect(status().isOk());
+	}
+
+	@Test
+	void canRetrieveSingleCustomer() throws Exception {
+		CreateCustomerRequest request = new CreateCustomerRequest();
+		request.setFirstName("Max");
+		request.setLastName("Mustermann");
+		request.setEmail("max.mustermann@example.org");
+
+		Customer customer = customerService.create(request);
+		String urlPath = "/v1/customer/" + customer.getId();
+		String token = "Bearer " + jwtTokenProvider.generateToken();
+
+		mockMvc.perform(get(urlPath).header("Authorization", token))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+				.andExpect(jsonPath("$.lastName").value(customer.getLastName()))
+				.andExpect(jsonPath("$.email").value(customer.getEmail()));
 	}
 
 }
