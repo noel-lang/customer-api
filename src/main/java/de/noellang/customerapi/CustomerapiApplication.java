09bb8afcb84dc126da12156d3f8b408235fd9811
Noel Lang <noel@lang-technology.de>
2021-04-21 12:16:44 +0200

    Initial Commit

---
+++ src/main/java/de/noellang/customerapi/CustomerapiApplication.java
@@ -0,0 +1,13 @@
+package de.noellang.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerapiApplication.class, args);
+	}
+
+}
