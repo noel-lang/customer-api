ac26a36039da7bc0ba2de10cd4544e5c6ae18332
Noel Lang <noel@lang-technology.de>
2021-04-21 13:50:51 +0200

    Add basic jwt auth setup

--- src/main/java/de/noellang/customerapi/config/SecurityConfig.java
+++ src/main/java/de/noellang/customerapi/config/SecurityConfig.java
@@ -1,18 +1,48 @@
 package de.noellang.customerapi.config;
 
+import de.noellang.customerapi.security.JwtAuthenticationEntryPoint;
+import de.noellang.customerapi.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/v1/**").permitAll();
-		http.cors().and().csrf().disable();
+		http
+				.authorizeRequests()
+				.antMatchers("/v1/auth/**")
+				.permitAll()
+				.and()
+				.cors()
+				.and()
+				.csrf()
+				.disable()
+				.exceptionHandling()
+				.authenticationEntryPoint(unauthorizedHandler)
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.anyRequest()
+				.authenticated();
+
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
